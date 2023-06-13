1b284ffe5333cbb7cbf2d07d26deb499b4d2c33a
d6felix <d6felix@gmail.com>
2023-06-13 15:57:07 +0300

    added login header

---
+++ src/components/LoginHeader/LoginHeader.tsx
@@ -0,0 +1,14 @@
+import styles from "./LoginHeader.module.scss";
+
+export function LoginHeader() {
+	return (
+		<header>
+			<h1>Ашайкин Филипп</h1>
+			<a href="">Telegram</a>
+			<a href="">GitHub</a>
+			<a href="">Resume</a>
+		</header>
+	);
+}
+
+export default LoginHeader;
