172ba66d821619f23459acd98c2674fcddf81f32
d6felix <d6felix@gmail.com>
2023-06-13 20:07:47 +0300

    added style to FormProgress

--- src/components/FormProgress/FormProgress.tsx
+++ src/components/FormProgress/FormProgress.tsx
@@ -1,9 +1,66 @@
+import classNames from "classnames";
+import styles from "./FormProgress.module.scss";
+import CheckMark from "@assets/CheckMark.svg";
+
 type FormProgressProps = {
 	step: number;
 };
 
 export function FormProgress({ step }: FormProgressProps) {
-	return <div>{step}</div>;
+	return (
+		<div className={classNames(styles.formProgress)}>
+			<div
+				data-step={step}
+				className={classNames(styles.formProgress__container_progress)}
+			>
+				{step > 1 ? (
+					<img
+						src={CheckMark}
+						alt="chekmark"
+						className={classNames(
+							styles.formProgress__step_completed,
+							styles.formProgress__step
+						)}
+					/>
+				) : (
+					<div
+						className={classNames(
+							step === 1 ? styles.formProgress__step_current : "",
+							styles.formProgress__step
+						)}
+					/>
+				)}
+				{step > 2 ? (
+					<img
+						src={CheckMark}
+						alt="chekmark"
+						className={classNames(
+							styles.formProgress__step_completed,
+							styles.formProgress__step
+						)}
+					/>
+				) : (
+					<div
+						className={classNames(
+							step === 2 ? styles.formProgress__step_current : "",
+							styles.formProgress__step
+						)}
+					/>
+				)}
+				<div
+					className={classNames(
+						step === 3 ? styles.formProgress__step_current : "",
+						styles.formProgress__step
+					)}
+				/>
+			</div>
+			<div className={classNames(styles.formProgress__container_labels)}>
+				<p className={classNames(styles.formProgress__label)}>1</p>
+				<p className={classNames(styles.formProgress__label)}>2</p>
+				<p className={classNames(styles.formProgress__label)}>3</p>
+			</div>
+		</div>
+	);
 }
 
 export default FormProgress;
--- src/pages/form/FormPage.tsx
+++ src/pages/form/FormPage.tsx
@@ -1,14 +1,15 @@
-import "./FormPage.scss";
 import { CustomForm } from "@components/CustomForm";
 import { useAppSelector } from "@hooks/reduxHooks";
 import { selectFormStep } from "@features/formStep/formStepSlice";
 import { FormProgress } from "@components/FormProgress";
+import styles from "./FormPage.module.scss";
+import classNames from "classnames";
 
 export function FormPage() {
 	const step = useAppSelector(selectFormStep);
 
 	return (
-		<div>
+		<div className={classNames(styles.formPage)}>
 			<FormProgress step={step} />
 			<CustomForm />
 		</div>
