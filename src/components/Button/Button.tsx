99a3e81688638ddec2880c6fafe1a61d60779e9f
d6felix <d6felix@gmail.com>
2023-06-14 19:59:47 +0300

    button styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,12 +5,25 @@ import styles from "./Button.module.scss";
 export type ButtonProps = React.PropsWithChildren<{
 	children: React.ReactNode;
 	className?: string;
+	style?: "solid" | "border";
 }> &
 	React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const Button = ({ children, className = "", ...props }: ButtonProps) => {
+export const Button = ({
+	children,
+	className = "",
+	style = "solid",
+	...props
+}: ButtonProps) => {
 	return (
-		<button {...props} className={classNames(styles.button, className)}>
+		<button
+			{...props}
+			className={classNames(
+				styles.button,
+				className,
+				styles.button.concat("_", style)
+			)}
+		>
 			<div>{children}</div>
 		</button>
 	);
