87e0e9848c072787dffb266b9cea3cb2e81e85ad
d6felix <d6felix@gmail.com>
2023-06-15 09:48:04 +0300

    added login header styles

--- src/components/LoginHeader/LoginHeader.tsx
+++ src/components/LoginHeader/LoginHeader.tsx
@@ -1,12 +1,27 @@
+import classNames from "classnames";
 import styles from "./LoginHeader.module.scss";
+import { useMemo } from "react";
+import FolderIcon from "@assets/FolderIcon.svg";
 
 export function LoginHeader() {
+	const firstName = "Филипп";
+	const lastName = "Ашайкин";
+	const userImg = useMemo(() => {
+		return lastName.charAt(0).concat(firstName.charAt(0));
+	}, []);
+
 	return (
-		<header>
-			<h1>Ашайкин Филипп</h1>
-			<a href="">Telegram</a>
-			<a href="">GitHub</a>
-			<a href="">Resume</a>
+		<header className={classNames(styles.header)}>
+			<div className={classNames(styles.header__userImg)}>{userImg}</div>
+			<h1>{lastName.concat(" ", firstName)}</h1>
+			<img src={FolderIcon} alt="icon for personal link" />
+			<a href="https://t.me/felix2d6">Telegram</a>
+			<img src={FolderIcon} alt="icon for personal link" />
+			<a href="https://github.com/d6felix">GitHub</a>
+			<img src={FolderIcon} alt="icon for personal link" />
+			<a href="https://hh.ru/resume/7fd12e42ff0af153d50039ed1f37616e324648">
+				Resume
+			</a>
 		</header>
 	);
 }
