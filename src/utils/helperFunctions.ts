04f44b806c46d0219f60861bcac5c5b8e6a6d1b0
d6felix <d6felix@gmail.com>
2023-06-17 18:17:12 +0300

    type fix

--- src/utils/helperFunctions.ts
+++ src/utils/helperFunctions.ts
@@ -2,10 +2,11 @@ export function capitalizeFirstLetter(string: string) {
 	return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export function phonenumberTransform(phoneNumber: number | string) {
+export function phonenumberTransform(phoneNumber: number | string): number {
 	if (typeof phoneNumber === "number") {
 		return phoneNumber;
 	} else if (typeof phoneNumber === "string") {
 		return +phoneNumber.replace(/\D/g, "").slice(1);
 	}
+	return phoneNumber;
 }
