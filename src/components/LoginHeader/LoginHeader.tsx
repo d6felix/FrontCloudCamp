ba590300b40b8a425d4df4f992124dcc897de9d0
d6felix <d6felix@gmail.com>
2023-06-23 13:29:45 +0300

    epdated semantic

--- src/components/LoginHeader/LoginHeader.tsx
+++ src/components/LoginHeader/LoginHeader.tsx
@@ -13,23 +13,29 @@ export function LoginHeader() {
 			<h1 className={styles.header__title}>
 				{lastName.concat(" ", firstName)}
 			</h1>
-			<span className={styles.header__linkContainer}>
-				<a href="https://t.me/felix2d6" className={styles.header__link}>
-					<img src={FolderIcon} alt="icon for personal link" />
-					<span>Telegram</span>
-				</a>
-				<a href="https://github.com/d6felix" className={styles.header__link}>
-					<img src={FolderIcon} alt="icon for personal link" />{" "}
-					<span>GitHub</span>
-				</a>
-				<a
-					href="https://hh.ru/resume/7fd12e42ff0af153d50039ed1f37616e324648"
-					className={styles.header__link}
-				>
-					<img src={FolderIcon} alt="icon for personal link" />
-					<span>Resume</span>
-				</a>
-			</span>
+			<ul className={styles.header__linkContainer}>
+				<li>
+					<a href="https://t.me/felix2d6" className={styles.header__link}>
+						<img src={FolderIcon} alt="icon for personal link" />
+						<span>Telegram</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://github.com/d6felix" className={styles.header__link}>
+						<img src={FolderIcon} alt="icon for personal link" />{" "}
+						<span>GitHub</span>
+					</a>
+				</li>
+				<li>
+					<a
+						href="https://hh.ru/resume/7fd12e42ff0af153d50039ed1f37616e324648"
+						className={styles.header__link}
+					>
+						<img src={FolderIcon} alt="icon for personal link" />
+						<span>Resume</span>
+					</a>
+				</li>
+			</ul>
 		</header>
 	);
 }
