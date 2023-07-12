27ed6a97c6966b3791b7dec1de3a5f91e9b7975d
d6felix <d6felix@gmail.com>
2023-07-12 14:22:57 +0300

    better form steps

--- src/components/CustomForm/CustomForm.tsx
+++ src/components/CustomForm/CustomForm.tsx
@@ -3,16 +3,16 @@ import { FormPart2 } from "./FormPart2/FormPart2";
 import { FormPart3 } from "./FormPart3/FormPart3";
 import { useAppSelector, selectFormStep } from "~store";
 
+const formParts = [
+	<FormPart1 key={1} />,
+	<FormPart2 key={2} />,
+	<FormPart3 key={3} />,
+];
+
 export function CustomForm() {
 	const step = useAppSelector(selectFormStep);
 
-	return (
-		<>
-			{step === 1 && <FormPart1 />}
-			{step === 2 && <FormPart2 />}
-			{step === 3 && <FormPart3 />}
-		</>
-	);
+	return <>{formParts[step - 1]}</>;
 }
 
 export default CustomForm;
